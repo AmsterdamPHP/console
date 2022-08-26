9dd4d3687e4280dba243760e797e56ffcd4fc06b
Rafael Dohms <rdohms@gmail.com>
2022-08-26 20:53:28 +0200

    Fix CircleCI Config

--- deploy.php
+++ deploy.php
@@ -12,7 +12,7 @@ set('writable_dirs', []);
 // Configure servers
 server('production', 'amsterdamphp.nl')
     ->user('phpamst01')
-    ->identityFile()
+    ->identityFile(null, '~/.ssh/id_rsa_0049b0d3413599f2a7d1024602070609')
     ->env('deploy_path', '/data/www/console');
 
 after('deploy:update_code', 'deploy:shared');
